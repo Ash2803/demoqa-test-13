7f21edbe05cfd6693791b679e64d5888c3b45edd
Ashot Abazyan <ashot.93@mail.ru>
2022-05-30 01:39:55 +0300

    added a test of registration form

---
+++ src/test/java/test/FormTest.java
@@ -0,0 +1,66 @@
+package test;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class FormTest {
+    String name = "Ashot";
+    String lastName = "Abazyan";
+    String userEmail = "a.abazyan@bk.ru";
+    String address1 = "Address1";
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void successfulTest() {
+        open("/automation-practice-form");
+        executeJavaScript("$('footer').remove()");
+        executeJavaScript("$('#fixedban').remove()");
+        $("[id = firstName").setValue(name);
+        $("[id = lastName]").setValue(lastName);
+        $("[id = userEmail]").setValue(userEmail);
+        $("#genterWrapper").$(byText("Male")).click();
+        $("[id = userNumber]").setValue("1234567890");
+        $("[id = dateOfBirth]").click();
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("April");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(byText("28")).click();
+        $(byText("Reading")).click();
+        $("#subjectsInput").sendKeys("E");
+        $(byText("English")).click();
+        $("#uploadPicture").uploadFile(new File("C:\\Users\\Ash\\Desktop\\demoqa-test-13\\" +
+                "src\\recources\\image.jpg"));
+        $("#currentAddress").setValue(address1);
+        $("#state").click();
+        $(byText("Rajasthan")).click();
+        $("#city").click();
+        $(byText("Jaipur")).click();
+        $("#submit").click();
+
+        $(".modal-body").shouldHave(
+                text("Ashot Abazyan"),
+                text("a.abazyan@bk.ru"),
+                text("Male"),
+                text("1234567890"),
+                text("28 March,1993"),
+                text("English"),
+                text("Reading"),
+                text("image.jpg"),
+                text("Address1"),
+                text("Rajasthan Jaipur")
+        );
+    }
+}
